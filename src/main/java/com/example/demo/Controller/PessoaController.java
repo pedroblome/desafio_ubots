59e11b0e9238a38c1f231dcded0191e76e3d03e3
pedroblome <joaopedroblome78@gmail.com>
2023-09-26 17:40:09 -0300

    end points finalizados

--- src/main/java/com/example/demo/Controller/PessoaController.java
+++ src/main/java/com/example/demo/Controller/PessoaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,13 @@ public class PessoaController {
     @PostMapping("/criar")
     public ResponseEntity<?> criarPessoa(@RequestBody Pessoa pessoa) {
         Pessoa newPessoa = pessoaService.criarPessoa(pessoa);
+        return ResponseEntity.ok().body(newPessoa);
+
+    }
+
+    @PutMapping("/editar")
+    public ResponseEntity<?> editarPessoa(@RequestBody Pessoa pessoa) {
+        Pessoa newPessoa = pessoaService.editarPessoa(pessoa);
         return ResponseEntity.ok().body(newPessoa);
 
     }
