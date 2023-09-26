992bd9181b8d22f6c519213ea630e6497ac18361
pedroblome <joaopedroblome78@gmail.com>
2023-09-26 01:05:36 -0300

    criado metodos de criar pessoa e endereco

--- src/main/java/com/example/demo/Service/EnderecoService.java
+++ src/main/java/com/example/demo/Service/EnderecoService.java
@@ -1,5 +1,42 @@
 package com.example.demo.Service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.Endereco;
+import com.example.demo.Model.Pessoa;
+import com.example.demo.Repositoy.EnderecoRepository;
+import com.example.demo.Repositoy.PessoaRepository;
+
+@Service
 public class EnderecoService {
-    
+
+    @Autowired
+    EnderecoRepository enderecoRepository;
+
+    @Autowired
+    PessoaRepository pessoaRepository;
+
+    private void validarEndereco(Endereco endereco) {
+        Optional<Pessoa> pessoa = pessoaRepository.findById(endereco.getPessoa().getId());
+
+        if (pessoa == null) {
+            throw new IllegalArgumentException("Campo id_pessoa nao pertence a nenhuma pessoa no sistema.");
+        }
+
+        else if (endereco.getCep() == null || endereco.getCidade() == null || endereco.getLogadouro() == null
+                || endereco.getNumero() == null) {
+            throw new IllegalArgumentException("Cep, Cidade, Logradouro e Numero são obrigatorios.");
+        }
+    }
+
+    private Endereco criarEndereco(Endereco endereco) {
+        validarEndereco(endereco);
+        Endereco newEndereco = enderecoRepository.save(endereco);
+        return newEndereco;
+
+    }
+
 }
--- src/main/java/com/example/demo/Service/PessoaService.java
+++ src/main/java/com/example/demo/Service/PessoaService.java
@@ -1,5 +1,28 @@
 package com.example.demo.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.Pessoa;
+import com.example.demo.Repositoy.PessoaRepository;
+
+@Service
 public class PessoaService {
-    
+
+    @Autowired
+    PessoaRepository pessoaRepository;
+
+
+    private void validarPessoa(Pessoa pessoa) {
+        if (pessoa.getNome() == null || pessoa.getDataNascimento() == null) {
+            throw new IllegalArgumentException("Nome e data de nascimento são campos obrigatórios.");
+        }
+    }
+
+    private Pessoa criarPessoa(Pessoa pessoa){
+        validarPessoa(pessoa);
+        Pessoa newPessoa = pessoaRepository.save(pessoa);
+        return newPessoa;
+     
+    }
 }
